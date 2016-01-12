41533e1d53ecd79c52948d8d6e49e9c37244e840
Samuel Ng <sam@infinitely.io>
2016-01-12 16:08:43 -0700

    basic command-line interface

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	Run(os.Args)
+}
+
+// Run command line tool
+func Run(args []string) {
+
+	app := cli.NewApp()
+
+	app.Name = "icmp-tunnel"
+	app.Usage = "Transparently tunnel your IP traffic through ICMP echo and reply packets"
+
+	verboseFlag := cli.BoolFlag{
+		Name:  "verbose, v",
+		Usage: "Enable verbose logging",
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "server",
+			Usage:  "Runs server",
+			Action: RunServer,
+			Flags:  []cli.Flag{verboseFlag},
+		},
+		{
+			Name:   "client",
+			Usage:  "Runs client",
+			Action: RunClient,
+			Flags:  []cli.Flag{verboseFlag},
+		},
+	}
+
+	app.Run(args)
+
+}
+
+// RunServer runs server-side daemon
+func RunServer(c *cli.Context) {
+}
+
+// RunClient runs client-side daemon
+func RunClient(c *cli.Context) {
+}
