5083b0cd9cb964cf15adc58db14d46cd298d9c4b
Samuel Ng <sam@infinitely.io>
2016-01-15 21:04:31 -0700

    update server daemon

--- server.go
+++ server.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/codegangsta/cli"
+)
+
+// RunServer runs server-side daemon
+func RunServer(c *cli.Context) {
+
+	// creates a tap interface
+	iface, err := TapInterface()
+
+	if err != nil {
+		log.Printf("creating interface error: %v\n", err)
+	}
+
+	iface.ParseIP(10, 0, 40, 1)
+	iface.Up()
+
+	data := make(chan []byte, 8)
+
+	go func() {
+		for {
+			buffer := make([]byte, 1522)
+			_, err := iface.Read(buffer)
+			if err == nil {
+				data <- buffer
+			}
+		}
+	}()
+
+	fmt.Println(iface.Name())
+
+	for {
+		select {
+		case buffer := <-data:
+			if buffer != nil {
+			}
+			// fmt.Println(buffer[:])
+		}
+	}
+}
