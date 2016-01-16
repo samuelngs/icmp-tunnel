f517f3c1cd7a5a23d9eec0663da306093019a7da
Samuel Ng <sam@infinitely.io>
2016-01-15 21:03:47 -0700

    improve network interface

--- interface.go
+++ interface.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"os"
+	"os/exec"
 	"strings"
 	"syscall"
 	"unsafe"
@@ -17,8 +20,9 @@ type IfReq struct {
 // Interface - the network interfaces
 type Interface struct {
 	name   string
-	class  *InterfaceType
+	class  interfaces
 	device *os.File
+	ipnet  net.IPNet
 }
 
 // Name returns Interface name
@@ -27,17 +31,45 @@ func (i *Interface) Name() string {
 }
 
 // Class returns Interface type
-func (i *Interface) Class() *InterfaceType {
+func (i *Interface) Class() interfaces {
 	return i.class
 }
 
+// Write implements io.Writer interface.
+func (i *Interface) Write(p []byte) (int, error) {
+	return i.device.Write(p)
+}
+
+// Read implements io.Reader interface.
+func (i *Interface) Read(p []byte) (int, error) {
+	return i.device.Read(p)
+}
+
+// ParseIP parses ip address
+func (i *Interface) ParseIP(a, b, c, d byte) {
+	self := net.IPv4(a, b, c, d)
+	mask := self.DefaultMask()
+	i.ipnet = net.IPNet{IP: self, Mask: mask}
+}
+
+// Up brings interface up/online
+func (i *Interface) Up() error {
+	if err := exec.Command("ip", "link", "set", i.name, "up").Run(); err != nil {
+		return err
+	}
+	if err := exec.Command("ip", "addr", "add", i.ipnet.String(), "dev", i.name).Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
 // NewInterface creates new interface
-func NewInterface(name string, class *InterfaceType, device string, flags uint16) (*Interface, IError) {
+func NewInterface(name string, class interfaces, device string, flags uint16) (*Interface, IError) {
 	file, err := os.OpenFile(device, os.O_RDWR, 0)
 	if err != nil {
 		return nil, err
 	}
-	ifr := &IfReq{}
+	ifr := IfReq{}
 	ifr.Flags = flags
 	copy(ifr.Name[:], name)
 	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, file.Fd(), uintptr(syscall.TUNSETIFF), uintptr(unsafe.Pointer(&ifr))); err != 0 {
@@ -49,11 +81,42 @@ func NewInterface(name string, class *InterfaceType, device string, flags uint16
 }
 
 // TapInterface creates tap interface
-func TapInterface() (*Interface, IError) {
-	return NewInterface("tap", InterfaceTypeTap, "/dev/net/tun", 0x0002|0x1000)
+func TapInterface(names ...string) (*Interface, IError) {
+	var name string
+	for _, val := range names {
+		name = val
+		break
+	}
+	return NewInterface(name, InterfaceTypeTap, "/dev/net/tun", 0x0002|0x1000)
 }
 
 // TunInterface creates tun interface
-func TunInterface() (*Interface, IError) {
-	return NewInterface("tun", InterfaceTypeTun, "/dev/net/tun", 0x0001|0x1000)
+func TunInterface(names ...string) (*Interface, IError) {
+	var name string
+	for _, val := range names {
+		name = val
+		break
+	}
+	return NewInterface(name, InterfaceTypeTun, "/dev/net/tun", 0x0001|0x1000)
+}
+
+// IPAddRoute to add route via ip command
+func IPAddRoute(dest, route, iface string) IError {
+	scmd := fmt.Sprintf("ip -4 r a %s via %s dev %s", dest, route, iface)
+	cmd := exec.Command("sh", "-c", scmd)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// IPDeleteRoute to remove route via ip command
+func IPDeleteRoute(dest string) IError {
+	sargs := fmt.Sprintf("-4 route del %s", dest)
+	args := strings.Split(sargs, " ")
+	cmd := exec.Command("ip", args...)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
 }
